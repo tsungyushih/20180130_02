b71d9233ad10ad4327f6cbe641f7295b9f5458da
Frank <Frank@hotmail.com>
2018-01-30 15:52:39 +0800

    Initial commit

---
+++ app/src/main/java/com/frandog/a20180130_02/MapsActivity.java
@@ -0,0 +1,49 @@
+package com.frandog.a20180130_02;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sanchung = new LatLng(25.0621, 121.4979);        //巨匠經緯度
+        LatLng loc101 = new LatLng(25.0336, 121.5646);      //台北101經緯度
+        mMap.addMarker(new MarkerOptions().position(sanchung).title("巨匠三重認證"));
+        mMap.addMarker(new MarkerOptions().position(loc101).title("台北101"));
+        //放大縮放倍率
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sanchung, 13));
+    }
+}
